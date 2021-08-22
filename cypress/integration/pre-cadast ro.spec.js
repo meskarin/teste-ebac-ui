1c701dbfe7193d9bcfdef0f8ccd2542cb30ec8e1
meskarin <rafael.ide@outlook.com>
2021-08-22 04:06:06 -0300

    Alteração do teste Pré-cadastro e Produtos

--- cypress/integration/pre-cadast ro.spec.js
+++ cypress/integration/pre-cadast ro.spec.js
@@ -8,13 +8,18 @@ beforeEach(() => {
 });
 
     it('Deve completar o pré ca dastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type(faker.internet.password())
+        let fakerName = faker.name.firstName()
+        let fakerEmail = faker.internet.email(fakerName)
+        let fakerLastName = faker.name.lastName()
+        let fakerPassword = faker.internet.password()
+
+        cy.get('#reg_email').type(fakerEmail)
+        cy.get('#reg_password').type(fakerPassword)
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain.text','Olá')        
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(fakerName)
+        cy.get('#account_last_name').type(fakerLastName)
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
 
---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,31 @@
+/// <reference type="cypress"/>
+
+describe('Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            .contains("Atlas Fitness Tank")
+            .click()
+    });
+
+    it.only('Deve adicionar ao carrinh', () => {
+        var quantidade = 6
+
+        cy.get('[class="product-block grid"]')
+            .contains("Atlas Fitness Tank")
+            .click()
+        cy.get('.button-variable-item-M').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text')
+            .clear()
+            .type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Atlas Fitness Tank” foram adicionados no seu carrinho.')
+    });
+});
